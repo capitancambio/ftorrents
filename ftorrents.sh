fd59d8fa2be5fe891edbc94a8bca9d373e544504
capitancambio <capitan.cambio@gmail.com>
2014-04-15 18:18:18 +0100

    Get rid of file logging

--- ftorrents.sh
+++ ftorrents.sh
@@ -3,13 +3,7 @@ import ftorrents.config
 import ftorrents.downloader
 import logging
 logger = logging.getLogger("ftorrents")
-hand =  logging.FileHandler("/tmp/ftorrents.log")
 logger.setLevel(logging.DEBUG)
-hand.setLevel(logging.DEBUG)
-
-formatter = logging.Formatter("[%(name)s] - %(levelname)s - %(message)s")
-hand.setFormatter(formatter)
-logger.addHandler(hand)
 hand = logging.StreamHandler()
 hand.setLevel(logging.DEBUG);
 logger.addHandler(hand);
